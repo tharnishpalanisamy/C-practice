d37536e43bdeb007e2e405d4cf608435847e6a7c
Tharnish Palanisamy <tharnishpalanisamy3@gmail.com>
2026-09-08 14:42:59 +0530

    basic recursion problems

--- C++/logical questions/isPrime.cpp
+++ C++/logical questions/isPrime.cpp
@@ -8,8 +8,8 @@ bool isPrime(int n ) {
     if(n ==2 ) {
         return true ; 
     }
-    for(int i = 2 ; i * i < n ; i ++) {
-        if ( n % 1 == 0 ) {
+    for(int i = 2 ; i * i <= n ; i ++) {
+        if ( n % i == 0 ) {
             return false ; 
         }
     }
